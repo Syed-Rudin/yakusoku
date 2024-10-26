4fd779adda0873b933b45623aa4a8820709016f7
Syed Rudin <syedrudin04@gmail.com>
2024-10-26 12:17:11 +0100

    Initial commit

---
+++ HackNotts/HackNottsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackNottsApp.swift
+//  HackNotts
+//
+//  Created by Syed Rudin on 26/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct HackNottsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HackNottsTests/HackNottsTests.swift
@@ -0,0 +1,17 @@
+//
+//  HackNottsTests.swift
+//  HackNottsTests
+//
+//  Created by Syed Rudin on 26/10/2024.
+//
+
+import Testing
+@testable import HackNotts
+
+struct HackNottsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
